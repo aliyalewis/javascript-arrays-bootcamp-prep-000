771651c84c035b1e2c962f0bda74aecf91512cbc
Aliya Lewis <aliyalewis5@gmail.com>
2019-04-04 23:56:03 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -31,7 +31,7 @@ function removeElementFromBeginningOfArray(array) {
   return array.slice(1)
 }
 
-function destructivelyRemoveElementFromEndOfArry(array) {
+function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop();
   return array
 }
