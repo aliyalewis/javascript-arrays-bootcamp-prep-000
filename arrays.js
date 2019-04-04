81057fe11cd32f400798a9721c5658c2eee8153f
Aliya Lewis <aliyalewis5@gmail.com>
2019-04-04 23:53:17 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element)
+  array.unshift(element);
   return array
 }
 
@@ -14,7 +14,8 @@ function addElementToEndOfArray(array, element) {
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  return array.push(element)
+  array.push(element);
+  return array
 }
 
 function accessElementInArray(array, index) {
